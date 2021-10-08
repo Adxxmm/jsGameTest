879aa966f033c4618afcf9ff854ff7308ee7587b
Adam <adxmbusinessyt@gmail.com>
2021-10-07 18:03:08 -0600

    added movement controls

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
+const MovementSpeed = 5
+
 var Width;
 var Height;
 var MainRect;
 var Canvas;
 var Color = ("rgb(0, 255, 0)");
+
+var XPos
+var YPos
 
 function setup() {
     Width = windowWidth * 0.8;
@@ -23,11 +28,34 @@ function setup() {
     FullScreenButton.mousePressed(fullScreenMode);
 
     FullScreenButton.parent("Buttons");
+
+    XPos = (Width / 2) - 50
+    YPos = (Height / 2) - 50
 }
 
 function draw() {
+    if (keyIsDown(LEFT_ARROW)) {
+        XPos -= MovementSpeed;
+        clear();
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+        XPos += MovementSpeed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        YPos += MovementSpeed;
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+        YPos -= MovementSpeed;
+    }
+
+    clear();
+    Canvas.background("gray");
+
     Color = color(Color);
-    MainRect = rect((Width / 2) - 50, (Height / 2) - 50, 100, 100);
+    MainRect = rect(XPos, YPos, 100, 100);
     MainRect.fill(Color);
 }
 
